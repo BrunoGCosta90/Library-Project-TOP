af0831b5012c6f3acb01b06482040fb5e2a9b1c1
BrunoGCosta90 <brunogallegocosta@hotmail.com>
2022-09-03 18:44:26 -0300

    Added values to radio buttons, added a container to show the book library, add functions to create books and book cards

--- script.js
+++ script.js
@@ -1,8 +1,30 @@
 let mylibrary = [];
 
-const addBook = document.getElementById('add').addEventListener('click', bookForm)
+const addBookFormButton = document.getElementById('add').addEventListener('click', bookFormModal)
+const addBookFormModal = document.getElementById('book-form-container')
+const closeBookForm = document.getElementById('close-button').addEventListener('click', closeForm)
+const openFormOverlay = document.getElementById('overlay')
+const addBookForm = document.getElementById('book-form')
+const addBookButton = document.getElementById('add-book').addEventListener('click', addBookToLibrary)
+const bookLibrary = document.getElementById('books')
 
-function bookForm(){
+// inputs
+
+const bookTitle = document.getElementById('book-title')
+const bookAuthor = document.getElementById('book-author')
+const bookPages = document.getElementById('book-pages')
+const bookRead = document.querySelector('input[name="book-read"]:checked')
+//const bookRead = document.querySelector('input[name="book-read"]')
+
+function bookFormModal(){
+    addBookFormModal.classList.add('open');
+    openFormOverlay.classList.add('open-form');
+}
+
+function closeForm(){
+    addBookForm.reset();
+    addBookFormModal.classList.remove('open');
+    openFormOverlay.classList.remove('open-form');
     
 }
 
@@ -13,11 +35,50 @@ function Book(title, author, pages, read){
     this.read = read
 }
 
-Book.prototype.info = function(){
-    return this.title + " by " + this.author + ", " + this.pages +
-    " pages, " + (this.read ? "already read." : "not read yet."); 
-}
+Book.prototype.addToLibrary = showLibrary;
 
 function addBookToLibrary(){
+    if (bookTitle.checkValidity() == true && bookAuthor.checkValidity() == true &&
+        bookPages.checkValidity() == true){
+            let read
+            if(bookRead.checked.valueOf() == false){
+                read = false
+            } else {
+                read = true
+            }
+            mylibrary.push(
+                new Book(
+                    bookTitle.value, 
+                    bookAuthor.value, 
+                    bookPages.value,
+                    read,
+                    )
+            )
+            mylibrary[mylibrary.length - 1].addToLibrary();
+            closeForm()
+    }
+    
+}
+
+function showLibrary(){
+    const bookCard = document.createElement('div')
+    bookLibrary.appendChild(bookCard)
+    bookCard.classList.add('book-card')
+    const bookCardTitle = document.createElement('p')
+    bookCard.appendChild(bookCardTitle)
+    bookCardTitle.textContent = this.title
+    const bookCardAuthor = document.createElement('p')
+    bookCard.appendChild(bookCardAuthor)
+    bookCardAuthor.textContent = this.author
+    const bookCardPages = document.createElement('p')
+    bookCard.appendChild(bookCardPages)
+    bookCardPages.textContent = this.pages
+    const bookCardRead = document.createElement('p')
+    bookCard.appendChild(bookCardRead)
+    if (this.read == false){
+        bookCardRead.textContent = "Not read yet."
+    } else if(this.read == true) {
+        bookCardRead.textContent = "Already read."
+    }
     
 }
